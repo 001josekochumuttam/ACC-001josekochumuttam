e9e5a526a0a5c1fea6ad9fb00248a2ad767d7607
001josekochumuttam <92168364+001josekochumuttam@users.noreply.github.com>
2021-10-08 15:10:32 +0100

    Add files via upload

---
+++ PYTHON Q1 TO 3.py
@@ -0,0 +1,9 @@
+firstName = input('enter your fisrt name:')
+secondName = input('enter your second name:')
+age = int(input('enter age:'))
+height = float(input('enter height:'))
+ageNextYear = age + 1
+
+print('welcome',firstName,secondName)
+print('youy in age will be',ageNextYear, 'in a year')
+print('your height is',height)
---
+++ python q4 to ..py
@@ -0,0 +1,79 @@
+num1=int(input('enter your number'))
+num2=int(input('enter your number'))
+print('your answer is:',num1 + num2)
+
+num1=float(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 + num2)
+
+num1=int(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 + num2)
+
+num1=int(input('enter your number'))
+num2=int(input('enter your number'))
+print('your answer is:',num1 - num2)
+
+num1=float(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 - num2)
+
+num1=int(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 - num2)
+
+num1=int(input('enter your number'))
+num2=int(input('enter your number'))
+print('your answer is:',num1 * num2)
+
+num1=float(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 * num2)
+
+num1=int(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 * num2)
+
+num1=int(input('enter your number'))
+num2=int(input('enter your number'))
+print('your answer is:',num1 / num2)
+
+num1=float(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 / num2)
+
+num1=int(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 / num2)
+
+num1=int(input('enter your number'))
+num2=int(input('enter your number'))
+print('your answer is:',num1 // num2)
+
+num1=float(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 // num2)
+
+num1=int(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 // num2)
+
+num1=int(input('enter your number'))
+num2=int(input('enter your number'))
+print('your answer is:',num1 % num2)
+
+num1=float(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 % num2)
+
+num1=int(input('enter your number'))
+num2=float(input('enter your number'))
+print('your answer is:',num1 % num2)
+
+
+
+
+
+
+
+
