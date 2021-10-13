015c7a9039edcd8396145014ac126fee1012fb3c
001josekochumuttam <92168364+001josekochumuttam@users.noreply.github.com>
2021-10-13 15:13:50 +0100

    Add files via upload

---
+++ python q7.py
@@ -0,0 +1,15 @@
+n1 = int(input('enter number'))
+
+lastDigit = n1%10
+print(lastDigit)
+
+#q8
+n1 = int(input('enter number'))
+
+lastDigit = n1%10
+
+if lastDigit%3 == 0:
+       print('n1 is multiple of three', lastDigit)
+
+else:
+    print('does not divide by three', lastDigit)
---
+++ python q9.py
@@ -0,0 +1,56 @@
+day = int(input('enter number between 1-7 '))
+
+if day == 1:
+    print('it is monday')
+
+elif day == 2:
+    print('it is tuesday')
+
+elif day == 3:
+    print('it is wednesday')
+
+elif day == 4:
+    print('it is thursday')
+
+elif day == 5:
+    print('it is friday')
+    
+elif day == 6:
+    print('it is saturday')
+    
+elif day == 7:
+    print('it is sunday')
+    
+else:
+    print('does not compute')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
