75d1e7841d77509383084854d38e6046e9c657a7
001josekochumuttam <92168364+001josekochumuttam@users.noreply.github.com>
2021-10-13 14:38:10 +0100

    Update python assignment 1 q6.py

--- python assignment 1 q6.py
+++ python assignment 1 q6.py
@@ -1,6 +1,14 @@
 units = int(input('enter units'))
 
-calculateBills(units):
-    if (units <= 100):
-        return units * 0;
+if (units <= 100):
+    print('no charge')
     
+elif(100 < units <= 200):
+    charge5Cents = units * 5
+    print('your bill is', charge5Cents)
+
+elif(200 < units):
+    charge10Cents = units * 10
+    print('your bill is', charge10Cents)
+
+
